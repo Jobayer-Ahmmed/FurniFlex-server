bb9c8922192078fa7083f6873dc691e3eff7593f
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-09-06 17:27:26 +0600

    add: basic structure of FurniFlex backend

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express"
+import applyMiddleWare from "./middlewares/applyMiddleWare.js"
+import rockingChairGet from "./routes/rockingChairRoutes/rockingChairGetRoute/index.js"
+
+
+
+const app =express()
+
+applyMiddleWare(app)
+app.use(rockingChairGet)
+
+
+app.get("/health", (req, res)=>{
+    res.send("FurniFlex server is running")
+})
+
+
+app.all("*", (req, res, next)=>{
+    const myErr = new Error(`The request URL is invalid : [${req.url}]`)
+    myErr.status=404
+    next(myErr)
+})
+
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500).json({message : err.message})
+})
+
+export default app
