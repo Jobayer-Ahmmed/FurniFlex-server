488b483ead6ffe71b5f7bb92b4c772f0cf9c6c64
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-09-06 17:33:33 +0600

    add: LoungeChair model

---
+++ src/models/LoungeChair.js
@@ -0,0 +1,16 @@
+
+
+import { Schema, model } from "mongoose";
+
+
+const loungeChairSchema = new Schema({
+    id: {type: String, required: true},
+    image: {type: String},
+    name: {type: String},
+    current_price: {type: Number},
+    previous_price: {type: Number},
+    description: {type: String}
+})
+
+const rockingChairModel = model("loungeChairCollectioin", loungeChairSchema)
+export default loungeChairModel
